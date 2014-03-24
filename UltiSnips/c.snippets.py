087535836132431c11c6efd7ba6ba6bc5566b3ea
John Szakmeister <john@szakmeister.net>
2014-03-24 17:36:44 -0400

    user/jszakmeister: add a template for .c and .h files

--- UltiSnips/c.snippets.py
+++ UltiSnips/c.snippets.py
@@ -15,3 +15,31 @@ from sniputil import abbr, babbr, wabbr
 bsnip("com", "/* comment */", r"""
 /* $1 */
 """, flags="b!")
+
+
+# Templates
+
+bsnip("template_c.c", ".c template", r"""
+#ifndef INCLUDED_`!p import re; res = re.sub('[-/]', '_', t[1].rsplit('.')[0]).upper()`
+#include "${1:`!v expand('%:t:r')`.h}"
+#endif
+
+$0
+""", flags="!")
+
+bsnip("template_c.h", ".h template", r"""
+#ifndef INCLUDED_${1:`!p import re; res = re.sub('[-/]', '_', vim.eval("expand('%:t:r')")).upper()`}
+#define INCLUDED_$1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+$0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INCLUDED_$1 */
+""", flags="!")
