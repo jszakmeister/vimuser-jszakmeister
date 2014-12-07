3459267e2a0980093275b8aa4b03ca2f33b16c03
John Szakmeister <john@szakmeister.net>
2014-12-07 06:59:20 -0500

    Make mail snippets pull in gitcommit snippets.
    This reduces a bit of redundancy, and it's not all that uncommon that
    what I'm editing is a git commit email.

--- UltiSnips/mail.snippets.py
+++ UltiSnips/mail.snippets.py
@@ -11,14 +11,4 @@ from sniputil import put
 from sniputil import snip, bsnip, wsnip
 from sniputil import abbr, babbr, wabbr
 
-bsnip("ab", "Acked-by: ...", r"""
-Acked-by: John Szakmeister <john@szakmeister.net>
-""")
-
-bsnip("sob", "Signed-off-by: ...", r"""
-Signed-off-by: John Szakmeister <john@szakmeister.net>
-""")
-
-bsnip("rb", "Reviewed-by: ...", r"""
-Reviewed-by: John Szakmeister <john@szakmeister.net>
-""")
+put("extends gitcommit\n")
