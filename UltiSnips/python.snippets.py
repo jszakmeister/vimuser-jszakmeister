c06224776390379fbdaa0caf652cdf8686f46002
John Szakmeister <john@szakmeister.net>
2024-08-06 15:57:22 -0400

    ultisnips: add a Python snippet for a subcommand cli tempate

--- UltiSnips/python.snippets.py
+++ UltiSnips/python.snippets.py
@@ -57,3 +57,62 @@ import gc; gc.set_debug(gc.DEBUG_LEAK)
 """)
 
 wsnip("fr", "from", "from ")
+
+bsnip("cli-sub", "subcommand cli template", r"""
+import argparse
+import sys
+
+from pathlib import Path
+
+from . import __version__ as VERSION
+
+
+def add_common_options(parser):
+    parser.add_argument(
+        "--traceback",
+        default=False,
+        action="store_true",
+        help="Print traceback of error.")
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=VERSION)
+
+
+def command_dummy(args):
+    print("Temporary dummy command.")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    add_common_options(parser)
+    parser.set_defaults(command=lambda args: parser.print_help())
+
+    subparsers = parser.add_subparsers()
+
+    cmd = subparsers.add_parser(
+        "dummy",
+        help="Temporary dummy command.")
+
+    add_common_options(cmd)
+
+    cmd.set_defaults(command=command_dummy)
+
+    args = parser.parse()
+
+    try:
+        args.command(args)
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        if args.traceback:
+            raise
+
+        print(f"ERROR: {e!s}", file=sys.stderr)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
+""")
