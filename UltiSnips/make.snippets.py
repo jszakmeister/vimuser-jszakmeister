6f5bb5788fd43d53655c42e8db65571058a2292f
John Szakmeister <john@szakmeister.net>
2024-09-27 12:01:32 -0400

    UltiSnips: add a couple of make snippets to dump variables

--- UltiSnips/make.snippets.py
+++ UltiSnips/make.snippets.py
@@ -8,6 +8,19 @@ from sniputil import put
 from sniputil import snip, bsnip, wsnip
 from sniputil import abbr, babbr, wabbr
 
+bsnip("dump-all", "dump all variables", r"""
+$(foreach v,$(sort $(.VARIABLES)),$(info $(v) = $($(v))))
+$0
+""")
+
+bsnip("dump-changed", "dump changed variables", r"""
+# Move this to top of file.
+VARS_OLD := $(sort $(.VARIABLES))
+
+$(foreach v,$(filter-out $(VARS_OLD) VARS_OLD, $(sort $(.VARIABLES))),$(info $(v) = $($(v))))
+$0
+""")
+
 bsnip("ifeq", "ifeq (...) / endif", r"""
 ifeq ($1,$2)
     $0
