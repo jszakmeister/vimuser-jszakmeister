05c81313ba14f334bede62ff8be73be53e4c66b0
John Szakmeister <john@szakmeister.net>
2024-12-02 05:58:39 -0500

    ultisnips: add a simple script template with no dependencies

--- UltiSnips/python.snippets.py
+++ UltiSnips/python.snippets.py
@@ -150,3 +150,113 @@ def hexdump(data):
 
 $0
 """)
+
+bsnip("cli-simple", "simple cli template for standalone scripts", r"""
+#!/usr/bin/env python3
+import argparse
+import logging
+import shutil
+import subprocess
+import sys
+
+from glob import glob
+from pathlib import Path
+from pprint import pprint
+
+
+COLORS = {
+    "yellow": "",
+    "reset": "",
+}
+
+
+def setup_say():
+    tput_path = shutil.which("tput")
+    if tput_path and sys.stdout.isatty():
+        COLORS["yellow"] = "".join(
+            [
+                run(tput_path, "bold"),
+                run(tput_path, "setaf", "3"),
+             ]
+        )
+        COLORS["reset"] = run(tput_path, "sgr0")
+
+
+def say(msg):
+    print(f"{COLORS['yellow']}{msg}{COLORS['reset']}")
+
+
+def run(*args, **kwargs):
+    show_cmd = kwargs.get('show_cmd', False)
+    shell = kwargs.get('shell', False)
+    encoding = kwargs.get('encoding', 'utf-8')
+
+    if shell:
+        command = ' '.join([shlex.quote(x) for x in args])
+    else:
+        command = args
+
+    if show_cmd:
+        print(' '.join(args))
+
+    check_ret = kwargs.get('check_ret', True)
+
+    p = subprocess.Popen(
+        command, shell=shell, encoding=encoding,
+        stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    output = p.communicate()
+    retcode = p.poll()
+    if check_ret and retcode:
+        raise ScriptError("Error executing: %r (%d, %s)" % (command, retcode, output))
+
+    return output[0]
+
+
+def run_git(*args, **kwargs):
+    path = kwargs.pop('path', None)
+
+    if path is not None:
+        path = str(path)
+
+        if path.endswith('.git'):
+            args = ['--git-dir=%s' % path] + list(args)
+        else:
+            args = ['--git-dir=%s/.git' % path, '--work-tree=%s' % path] + list(args)
+
+    command = ['git'] + list(args)
+    return run(*command, **kwargs).strip()
+
+
+def do_the_thing(args):
+    say("Do real work here.")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--traceback",
+        default=False,
+        action="store_true",
+        help="Print traceback of error.")
+
+    args = parser.parse_args()
+
+    setup_say()
+
+    try:
+        do_the_thing(args)
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        if args.traceback:
+            raise
+
+        print(f"ERROR: {e!s}", file=sys.stderr)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
+""")
